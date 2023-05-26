d1b71e06fb85d9d236df358057267e6155bea166
SilentDeath53 <48599339+SilentDeath53@users.noreply.github.com>
2023-05-26 16:00:57 +0300

    Create encrypter_decrypter.go

---
+++ Encrypting and Decrypting Data/encrypter_decrypter.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+func encrypt(key []byte, text string) (string, error) {
+	plaintext := []byte(text)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		return "", err
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	return base64.URLEncoding.EncodeToString(ciphertext), nil
+}
+
+func decrypt(key []byte, ciphertext string) (string, error) {
+	ciphertextBytes, err := base64.URLEncoding.DecodeString(ciphertext)
+	if err != nil {
+		return "", err
+	}
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+
+	if len(ciphertextBytes) < aes.BlockSize {
+		return "", fmt.Errorf("ciphertext is too short")
+	}
+
+	iv := ciphertextBytes[:aes.BlockSize]
+	ciphertextBytes = ciphertextBytes[aes.BlockSize:]
+
+	stream := cipher.NewCFBDecrypter(block, iv)
+	stream.XORKeyStream(ciphertextBytes, ciphertextBytes)
+
+	return string(ciphertextBytes), nil
+}
+
+func main() {
+	key := []byte("16-byte-key-here")
+	text := "Hello, World!"
+
+	encrypted, err := encrypt(key, text)
+	if err != nil {
+		fmt.Println("Encryption error:", err)
+		return
+	}
+	fmt.Println("Encrypted text:", encrypted)
+
+	decrypted, err := decrypt(key, encrypted)
+	if err != nil {
+		fmt.Println("Decryption error:", err)
+		return
+	}
+	fmt.Println("Decrypted text:", decrypted)
+}
